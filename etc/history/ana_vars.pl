4c3ddc7130894840c622caf5287fa6461a53fbdb
Arlindo M. da Silva, Jr <arlindo.dasilva@nasa.gov>
2025-02-26 09:29:55 -0500

    Work in progress

---
+++ etc/history/ana_vars.pl
@@ -0,0 +1,27 @@
+
+#
+# Prognostic variables
+#
+          'PHIS'   , 'AGCM'       ,
+          'SLP'    , 'DYN'        ,
+          'DELP'   , 'DYN'        ,
+          'PS'     , 'DYN'        ,
+
+          'U;V'    , 'DYN'        ,
+          'W'      , 'DYN'        ,
+          'OMEGA'  , 'DYN'        ,
+          'T'      , 'DYN'        ,
+          'ZL'     , 'DYN'        , 'H'   ,
+  
+          'Q'       , 'MOIST'     , 'QV'  ,
+  
+          'QITOT'   , 'AGCM'      , 'QI'  ,
+          'QLTOT'   , 'AGCM'      , 'QL'  ,
+          'QRTOT'   , 'MOIST'     , 'QR'  ,
+          'QSTOT'   , 'MOIST'     , 'QS'  ,
+          'QGRAUPEL', 'MOIST'     , 'QG'  ,
+  
+          'RH2'     , 'MOIST'     , 'RH'  ,
+          'O3'      , 'CHEMISTRY' ,
+
+  
---
+++ etc/pbl_obsys/README.md
@@ -0,0 +1,6 @@
+# Notional Observing System during a PBL Mission inn the mid 2030's
+
+The files in this directory to documenrt the nbotiuonal observing system
+expected to be vailable in the mid to late 2030's when a PBL mission
+would hypothetically fly.
+
